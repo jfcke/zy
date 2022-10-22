f1103df1ab63ef10c5aa7d2b1617f5678ca77692
zyz <116411467+jfcke@users.noreply.github.com>
2022-10-22 12:09:40 +0800

    C program

--- 程序设计/2022年10月22日/a b.c
+++ 程序设计/2022年10月22日/a b.c
@@ -4,9 +4,8 @@ int main () {
 	int a = 0;
 	int b = 0;
 
-	scanf("%d%d", &a, &b);
+	scanf("%d,%d", &a, &b);
+	printf("%d %d\n", a, b);
 
-	printf("%d%d\n", a, b);
-
-
+	return 0;
 }
---
+++ 程序设计/2022年10月22日/找钱plus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+	const int AMOUNT = 1000;
+	int price = 0;
+
+	printf("请输入金额（元）");
+	scanf("%d", &price);
+
+	int change = AMOUNT - price;
+
+	printf("找您%d元。", change);
+
+	return 0;
+}
